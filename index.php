f710db9428f1044b94d4637c678fca7373e57745
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-12-18 10:10:40 +0100

    .

--- index.php
+++ index.php
@@ -7,11 +7,9 @@
 </head>
 <body>
     <?php $var="la mia prima variabile php" ?>
-    <h1><?php $var ?></h1>
+    <h1><?php $var "prima stampa" ?></h1>
 
-    <?php $var="la mia varibile riassegnata 1 volta ?>
-    <h1><?php $var ?></h1>
-
-    
+    <?php $var="la mia varibile riassegnata 1 volta" ?>
+    <h1><?php "$var seconda stampa" ?></h1>   
 </body>
 </html>
