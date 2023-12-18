acef4c5d4ad01ff42b02376f794ea0f6b37632a2
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-12-18 09:56:58 +0100

    frst

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $var="la mia prima variabile php" ?>
+    <h1><?php $var ?></h1>
+
+    <?php $var="la mia varibile riassegnata 1 volta ?>
+    <h1><?php $var ?></h1>
+
+    
+</body>
+</html>
